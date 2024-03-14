4b6295a615bf195ada65b11f0267687cb2d6d23d
rohitsasmal <rohitsasmal1994@gmail.com>
2024-03-14 22:03:24 +0530

    Port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Initialize SQLite database
 const db = new sqlite3.Database('phone_numbers.db');
